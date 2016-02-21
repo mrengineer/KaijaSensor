743099ca164278753632f848acaee0c2e624dee0
Dima <dimitrybulkin@yandex.ru>
2016-02-21 13:49:07 +0300

    Only RX is fixed

--- Src/main.c
+++ Src/main.c
@@ -168,12 +168,14 @@ int main(void)
       - Hardware flow control disabled (RTS and CTS signals) */
   huart1.Instance        = USART1;
 
-  huart1.Init.BaudRate   = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits   = UART_STOPBITS_1;
-  huart1.Init.Parity     = UART_PARITY_ODD;
-  huart1.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-  huart1.Init.Mode       = UART_MODE_TX_RX;
+  huart1.Init.BaudRate   	= 9600;
+  huart1.Init.WordLength	= UART_WORDLENGTH_8B;
+  huart1.Init.StopBits   	= UART_STOPBITS_1;
+  huart1.Init.Parity     	= UART_PARITY_ODD;
+  huart1.Init.HwFlowCtl 	= UART_HWCONTROL_NONE;
+  huart1.Init.Mode 				= UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl 	= UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
 
   if (HAL_UART_Init(&huart1) != HAL_OK)
   {
